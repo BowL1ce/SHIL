083d832eb046546922d4dea0d332ea12f1d6f667
BowLice <150810031+BowL1ce@users.noreply.github.com>
2025-12-01 07:45:40 +0300

    h

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
-var chats = new Map();
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once(Events.ClientReady, (readyClient) => {
-	console.log(`Logged in as ${readyClient.user.tag}`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
+console.log(2 + 2)
